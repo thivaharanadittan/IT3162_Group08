21a71a251f79c23d83ae57418019e56929323be3
nethmir32 <nethmir32@gmail.com>
2024-05-07 13:13:41 +0530

    add feedback
    add feedback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,56 @@
-import './App.css';
+// import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './feedback/CSS/custom.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import Home from "./components/Home";
+import EmployeeHome from "./components/EmployeeManagement/EmployeeHome";
+import EmployeeLogin from "./components/EmployeeManagement/EmployeeLogin";
+import EmployeeSignup from "./components/EmployeeManagement/EmployeeSignup";
+
+import EmployeeProfileView from "./components/EmployeeManagement/EmployeeProfileView";
+import EmployeeViewDetails from "./components/EmployeeManagement/EmployeeViewDetails";
+import EmployeeEdit from "./components/EmployeeManagement/EmployeeEdit";
+import EmployeeAdminView from "./components/EmployeeManagement/EmployeeAdminView";
+import EmployeeAdminUpdate from "./components/EmployeeManagement/EmployeeAdminUpdate";
+
+import EmployeeSalaryManagement from "./components/EmployeeManagement/EmployeeSalaryManagement";
+import EmployeeLeaveManagement from "./components/EmployeeManagement/EmployeeLeaveManagement";
+
 import StickyHeader from './feedback/components/header';
 import FeedbackForm from './feedback/components/form';
 import Submissions from './feedback/components/submissions'
 
+
+
+
+import { useState } from "react";
+
 function App() {
   return (
-    
     <div className="App">
-      <StickyHeader/>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FeedbackForm/>}/>
+	<StickyHeader/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<EmployeeLogin />} />
+          <Route path="/signup" element={<EmployeeSignup />} />
+          <Route path="/employeemanagement" element={<EmployeeHome />} />
+          <Route path="/profile-view" element={<EmployeeProfileView />} />
+          <Route path="/view-details" element={<EmployeeViewDetails />} />
+          <Route path="/employee-edit:id" element={<EmployeeEdit />} />
+          <Route path="/admin-view" element={<EmployeeAdminView />} />
+          <Route path="/admin-update" element={<EmployeeAdminUpdate />} />
+          <Route path="/salary-management" element={<EmployeeSalaryManagement />} />
+          <Route path="/employee-leaving" element={<EmployeeLeaveManagement />} />
+		  
+		  <Route path="/" element={<FeedbackForm/>}/>
         <Route path="submissions" element={<Submissions />} />
         <Route path="submission/:id" element={<Submissions />} />
-      </Routes>
-    </BrowserRouter>
-      
+         
+        </Routes>
+      </Router>
     </div>
   );
 }
