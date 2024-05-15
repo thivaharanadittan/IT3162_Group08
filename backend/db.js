4a74047726c4e8e050652bcc98fbd59566c4a29c
NuhaNizam <nuhanizam2001@gmail.com>
2024-05-15 07:35:58 -0700

    Database Changed

--- backend/db.js
+++ backend/db.js
@@ -1,47 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose=require("mongoose");
+//var mongoURL = 'mongodb://localhost:27017/room_details';
+var mongoURL = 'mongodb://localhost:27017/HotelManagement';
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mydatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  bufferCommands: false,
-  serverSelectionTimeoutMS: 5000
+mongoose.connect(mongoURL ,{ useNewUrlParser: true, useUnifiedTopology: true });
+
+var connection=mongoose.connection
+
+connection.on('error' , ()=>{
+    console.log('Mongo DB connection failed')
 })
-.then(() => {
-  console.log('Connected to MongoDB');
+ 
+connection.on('connected',()=>{
+    console.log('Mongo DB connection Successfull')
 })
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
 
-// Define a schema for a collection
-const complaintSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  roomNo: String,
-  checkInDate: Date,
-  checkOutDate: Date,
-  complaint: String
-});
-
-
-// Define a model for the collection
-const Complaint = mongoose.model('Complaint', complaintSchema);
-
-// Example of creating a document (data) in the collection
-const newComplaint = new Complaint({
-  name: 'John Doe',
-  email: 'john@example.com',
-  roomNo: '101',
-  checkInDate: new Date(),
-  checkOutDate: new Date(),
-  complaint: 'No hot water in the room'
-});
-
-newComplaint.save()
-.then(() => {
-  console.log('Complaint saved successfully');
-})
-.catch((error) => {
-  console.error('Error saving complaint:', error);
-});
+module.exports = mongoose;
