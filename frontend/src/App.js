5edadcb0ea5d4cea063645df493b545dd48bd7d2
Suvarna09 <87079954+Suvarna09@users.noreply.github.com>
2024-05-15 15:40:17 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
+// App.js
 import React from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import BookingScreen from './screens/BookingScreen';
-import Register from './screens/Register';
-import Login from './screens/Login';
-import ProfileScreen from './screens/ProfileScreen';
-import LandingScreen from './screens/LandingScreen';
+//import Dashboard from './components/Dashboard';
+import { Routes,Route } from 'react-router-dom';
+import BookingForm from './components/BookingForm';
+import VehicleInfo from './components/VehicleInfo';
+import Feedback from './components/Feedback';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <NavBar />
-      <BrowserRouter>
-        <Routes>
-          {/* Use 'element' prop with the component imported correctly */}
-          <Route path='/home' element={<HomeScreen />} />
-          
-          <Route path='/book/:roomid/:fromDate/:toDate' element={<BookingScreen />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/profile' element={<ProfileScreen />} />
-          <Route path='/' element={<LandingScreen />} />
-          
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='BookingForm' element={<BookingForm/>}/>
+      <Route path='BookingForm/VehicleInfo' element={<VehicleInfo/>} />
+      <Route path='BookingForm/VehicleInfo/Feedback' element={<Feedback/>}/>
+    </Routes>
+
+  );
+};
 
 export default App;
