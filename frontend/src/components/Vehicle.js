50d8ec0ad995128139c10ad60b45ef4f6670f3a7
Suvarna09 <87079954+Suvarna09@users.noreply.github.com>
2024-05-15 15:40:00 +0530

    Create Vehicle.js

---
+++ frontend/src/components/Vehicle.js
@@ -0,0 +1,8 @@
+import car from '../source/car.jpeg';
+import van from '../source/van.jpeg';
+import bus from '../source/bus.png';
+ export const Vehicle = [
+  {  image: car,type: 'Car', capacity: 4, amenities: ['AC', 'WiFi'] },
+  {  image:van, type: 'Van', capacity: 8, amenities: ['AC', 'WiFi', 'TV'] },
+  {  image:bus,type: 'Bus', capacity: 20, amenities: ['AC', 'WiFi', 'TV', 'Restroom'] },
+];
